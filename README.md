6f61b379a5fc2829acc00514ac4615ca53bf19bb
kennyjoseph <josephkena@gmail.com>
2018-12-21 15:49:21 -0500

    adding PEW comparison

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Replication Materials Details
 
+[![DOI](https://zenodo.org/badge/162752654.svg)](https://zenodo.org/badge/latestdoi/162752654)
+
 This repository contains public replication materials for the paper "Fake News on Twitter During the 2016 U.S. Presidential Election."
 
-Please note that most of the code in this repository requires access to the restricted data release for this paper. For access to this data, please contact d.lazer@northeastern.edu.
+Please note that some of the code in this repository requires access to the restricted data release for this paper. For access to this data, please contact d.lazer@northeastern.edu.  Once you have access, you can simply place the required file into the data directory of the particular result you are replicating to finish the replication.
 
 # Structure of the Repository
 
@@ -48,3 +50,43 @@ pct_orange - percentage of all shares/exposures that were to/from orange sites
 pct_black - percentage of all shares/exposures that were to/from black sites
 pct_fake - percentage of all shares/exposures that were to/from any fake news site
 ```
+
+## Coexposure analysis
+
+### Code
+The script ```coexposure_network/analysis.R``` generates Figure 7 from the main text, as well as Figures S10 and S11 in the supplementary.
+
+### Data
+
+The file ```coexposure_network/data/site_to_site_connections.csv``` provides details on each site pair considered in the analysis, using the methods specified in Dianati et al., 2016.  The columns of the data are:
+
+```
+site_j - one site relevant to this edge
+site_i - the other site relevant to this edge
+x - total number of exposures common to the two sites
+weight_i, weight_j, q, p - used to compute significance of edge weights under the null model, see paper for details
+pval - the p-value of the observed edge under the null
+significance - the significance of the edge (the negative logarithm of the p-value)
+```
+
+The file ```coexposure_network/data/site_map.tsv``` just cleans up some of the sitenames. The file ```total_exposure_counts.csv``` gives the total number of exposures for each domain, and is just used for sizing for Figure 7.
+
+There are two files used in the script that are not available in this public data release:
+
+-  ```coexposure_network/data/user_to_domain_network.csv``` links an anonymized user ID (```panel_uid```) to a website (```website```) and gives the number of times the user was exposed to that website.  However, as it is individual level data, we do not share it here.
+- ```coexposure_network/data/fb_top500_domain_affl.csv``` is taken from the Bakshy et al. Facebook study, and must be requested from those authors. 
+
+
+## Representativeness (PEW)
+
+### Code
+The script ```compare_to_pew/analyze_representativeness_pew.R``` generates Figure S1 from the supplementary.
+
+### Data
+
+There are two files used in the script that are not available in this public data release:
+
+- ```compare_to_pew/data/pew_stats.csv``` contains data drawn directly from [PEW's 2016 Social Media update survey](http://www.pewinternet.org/dataset/march-2016-libraries/).  You will need to register for and then download the CSV release for this survey, and then rename it to ```pew_stats.csv```.
+
+- ```compare_to_pew/data/panel_stats.csv``` contains information on panel members party, sex, race_ethnicity, and age (anonymized). ***This file is contained only in the restricted release***.
+
---
+++ compare_to_pew/analyze_representativeness_pew.R
@@ -0,0 +1,100 @@
+library(survey)
+library(binom)
+library(dplyr)
+library(survey)
+
+######## Contained in restricted release***********
+panel <- fread("compare_to_pew/data/panel_stats.csv")
+######################################
+
+pew_data <- fread("compare_to_pew/data/pew_stats.csv")
+#Is Internet User
+pew_data[ , is_internet_user := eminuse == 1 | intmob == 1]
+
+# Is Twitter User
+pew_data[, is_twitter_user := ifelse(is.na(act112),NA, 
+                                     ifelse(act112 == 9, NA,
+                                            ifelse(act112 == 1, "Yes","No")))]
+
+#Party Score
+pew_data[, party_score := party]
+pew_data[party_score > 3, party_score := 4]
+pew_data[, party_score := mapvalues(party_score,c(1,2,3,4),c("Republican","Democrat","Independent","No Party/No Info"))]
+
+#Sex
+pew_data[, sex := mapvalues(sex,c(1,2),c("Male","Female"))]
+
+#Race
+pew_data[,race_ethnicity := ifelse(hisp == 1, 1, ifelse(race3m1 <3, race3m1+1, 4))]
+pew_data[, race_ethnicity := mapvalues(race_ethnicity, c(1,2,3,4), 
+                                       c("Hispanic","Caucasian","African-American","Other"))]
+
+# Is registered to vote
+pew_data[, vote_registered := ifelse(reg < 2, 1, ifelse(reg == 3, 0, NA))]
+
+
+v <- svydesign(ids=~1,data=pew_data,weights=~weight)
+
+# how many people are internet users in their sample
+svyciprop(~is_internet_user,v,method="logit")
+
+# how many internet using adults use twitter?
+svyciprop(~I(is_twitter_user=="Yes"),subset(v, !is.na(is_twitter_user)),method="logit")
+
+# how many of those on Twitter are registered to vote, likely at the address they are located at?
+svyciprop(~vote_registered,subset(v, !is.na(vote_registered) & is_twitter_user=="Yes"),method="logit")
+
+# Okay, lets now check if our sample is representative of voters on Twitter
+
+make_confint_table <- function(x){
+  tab <- data.table(table(x))
+  setnames(tab, c("name","N"))
+  tab <- tab[N > 0]
+  tab <- tab[,binom.confint(N,sum(tab$N),methods = "ac"),by=name][,.(name,mean,lower,upper)]
+  tab$Data <- "Panel"
+  return(tab)
+}
+
+gen_pew_table <- function(factor_var,data_subset){
+  pew <- data.frame(svymean(as.formula(paste0("~",factor_var)),data_subset, method="logit"))
+  pew$name <- sub(factor_var,"",rownames(pew))
+  pew$upper <- pew$mean + 2*pew$SE
+  pew$lower <- pew$mean - 2*pew$SE
+  pew$Data <- "PEW"
+  return(pew[,c("name","mean","lower","upper","Data")])
+}
+
+voters <- subset(v,!is.na(vote_registered) & vote_registered==1 & is_twitter_user == "Yes")
+
+dim(voters)
+
+# Party
+dim(subset(voters, party_score != "No Party/No Info"))
+pew_party <- gen_pew_table("party_score",subset(voters, party_score != "No Party/No Info"))
+panel_party <- make_confint_table(panel[party %in% pew_party$name]$party)
+party_dat <- rbind(panel_party,pew_party)
+
+# Sex
+pew_sex <- gen_pew_table("sex",voters)
+panel_sex <- make_confint_table(panel[sex %in% pew_sex$name]$sex)
+sex_dat <- rbind(pew_sex,panel_sex)
+
+# Race/Ethnicity
+pew_race <- gen_pew_table("race_ethnicity",voters)
+panel_race <- make_confint_table(panel$race_ethnicity)
+race_dat <- rbind(pew_race,panel_race)
+race_dat[,name := mapvalues(name, c("Hispanic","Caucasian","African-American","Other"),
+                          c("Hispanic","White","Black","Other"))]
+
+
+dat <- rbind(race_dat[name != "Other"],party_dat,sex_dat)
+dat$name <- factor(dat$name,levels=unique(dat$name))
+plt <- ggplot(dat, aes(name,mean,ymin=lower,ymax=upper,color=Data)) 
+plt <- plt + geom_pointrange(position=position_dodge(.5),size=1.) + coord_flip()
+plt <- plt + scale_y_continuous("Estimated Percent",labels=percent,limits=c(0,1))
+plt <- plt + my_custom_theme() +xlab("Demographic Characteristic")
+ggsave(plt,file="compare_to_pew/img/pew_rep.pdf",h=6,w=7)
+
+# Age
+smean.cl.boot(panel$age)
+svymean(~age,voters)
